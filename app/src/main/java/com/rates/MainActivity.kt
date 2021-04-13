b948448329bfa1be6a913265194a6d816997dd53
IvanGalitskiy <ivan.galitskiy8@gmail.com>
2021-04-13 19:02:19 +0300

    basic theme set up

--- app/src/main/java/com/rates/MainActivity.kt
+++ app/src/main/java/com/rates/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpActionBar()
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(findViewById(R.id.activity_main_toolbar))
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_close)
+        }
     }
 }
